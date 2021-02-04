4df557c7d9bdfc9bb5305c39db5979e2d3874993
Alex Cano <alexander.cano@seriously.com>
2021-02-04 15:44:28 -0800

    fix test to be dynamically comparing package versions

--- tests/test_dotcfg.py
+++ tests/test_dotcfg.py
@@ -1,5 +1,20 @@
-from dotcfg import __version__
+import os
+import subprocess
+
+import dotcfg
 
 
 def test_version():
-    assert __version__ == "0.1.0"
+    """
+    Tests that the version in the code matches the version
+    provided in the package information
+    """
+    current_version = dotcfg.__version__
+
+    cwd = os.path.join(os.path.dirname(__file__))
+    cmd = ["poetry", "version"]
+    # output will be b"dotcfg X.Y.Z\n"
+    poetry_version = subprocess.check_output(cmd, cwd=cwd).strip().decode()
+    package_version = poetry_version.replace("dotcfg ", "")
+
+    assert package_version == current_version
