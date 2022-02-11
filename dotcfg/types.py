213bc3c5901868c40e9fd47de4a686c4f0a60987
Alex Cano <alexander.cano@seriously.com>
2022-02-11 09:24:26 -0800

    update types for pathlib friendliness

--- dotcfg/types.py
+++ dotcfg/types.py
@@ -1,6 +1,4 @@
-from os import PathLike
+from pathlib import Path
 from typing import Union
 
-# Found the definition in stubs for the standard library. Goal is to
-# support the same things that can be passed to `open`
-StrPath = Union[str, PathLike[str]]
+StrPath = Union[str, Path]
